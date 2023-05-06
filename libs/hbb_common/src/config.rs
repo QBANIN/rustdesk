4366a54017aded654f4d420fe627dc4b6679b250
QBANIN <qbanin@gmail.com>
2023-05-06 20:53:22 +0200

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -82,14 +82,12 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &[
-    "rs-ny.rustdesk.com",
-    "rs-sg.rustdesk.com",
-    "rs-cn.rustdesk.com",
+    "rust.sobczyk.in",
 ];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+    _ => "fzEx7HVrn65wHUOu+p5KhU3OOvPyP8PdzM7MxL6NV1s=",
 };
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
